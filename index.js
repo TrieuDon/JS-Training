397745da69219108a563dd5ddb79e5c809102fb9
TrieuDon <don98tb@gmail.com>
2021-06-29 11:14:09 +0700

    update

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 let stuff = ["a", "b", "c", "d"]
-let option = prompt ("What do you want? ()C, R, U, D")
-let Choice = ["C", "R", "U", "D", "c"]
-if (option )
-if (option == "C" || option == "c") {
+let CRUD = (e) => {
+    let option = prompt("What do you want? ()C, R, U, D");
     option.toUpperCase();
-    let name = prompt ("Insert your name")
-    stuff.push(name)
-    console.log(stuff)
-}
-else if (option == "R") {
-    console.log ('this is your Staff')
-}
-else if (option == "U") {
-    let positionNeedUpdate = Number(prompt ('Insert position that need updating'))
-    let nameUpdate = prompt ('Insert name to update')
-    stuff[positionNeedUpdate] = nameUpdate;
-}
-else if (option == "D") {
-    let positionNeedDeleting = Number(prompt("Insert name that needs deleting"));
-    if (i > 0 && i < stuff.length) {
-        stuff.splice(i, 1)
+    let choice = ["C", "R", "U", "D"];
+    if (choice.includes(option)) {
+        if (option == "C") {
+            let name = prompt("Insert your name")
+            stuff.push(name)
+            console.log(stuff)
+        }
+        else if (option == "R") {
+            console.log('this is your Staff')
+        }
+        else if (option == "U") {
+            let positionNeedUpdate = Number(prompt('Insert position that need updating') -1)
+            if ( 0 <= positionNeedUpdate && positionNeedUpdate < stuff.length) {
+                let nameUpdate = prompt('Insert Name to update')
+                stuff[positionNeedUpdate] = nameUpdate;
+                console.log(stuff)
+            }
+            else {
+                alert('Position not available');
+                CRUD("U");
+            }
+        }
+        else if (option == "D") {
+            let positionNeedDeleting = Number(prompt("Insert position that needs deleting") - 1);
+            if (positionNeedDeleting > 0 && positionNeedDeleting < stuff.length) {
+                stuff.splice(positionNeedDeleting, 1)
+                console.log(stuff)
+            }
+            else {
+                alert("not valid number. Please try again")
+            }   
+        }
     }
     else {
-        alert ("Name not found")
-        prompt ("Insert name that needs deleting")
+        alert("Invalid Selection. Please Choose again");
+        CRUD();
+        
     }
 }
-stuff.forEach(i => console.log(stuff[i]))
