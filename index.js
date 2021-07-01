fec41b07cbf77f0190f347f0d081bbfbbc70452d
TrieuDon <don98tb@gmail.com>
2021-07-01 11:28:23 +0700

    bai tap DOM

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
-let stuff = ["a", "b", "c", "d"]
-let CRUD = () => {
-    let option = prompt("What do you want? ()C, R, U, D");
-    option = option.toUpperCase();
-    let choice = ["C", "R", "U", "D"];
-    if (choice.includes(option)) {
-        if (option == "C") {
-            let name = prompt("Insert your name")
-            stuff.push(name)
-            console.log(stuff)
-        }
-        else if (option == "R") {
-            console.log('this is your Staff')
-        }
-        else if (option == "U") {
-            let positionNeedUpdate = Number(prompt('Insert position that need updating') -1)
-            if ( 0 <= positionNeedUpdate && positionNeedUpdate < stuff.length) {
-                let nameUpdate = prompt('Insert Name to update')
-                stuff[positionNeedUpdate] = nameUpdate;
-                console.log(stuff)
-            }
-            else {
-                alert('Position not available');
-            }
-        }
-        else if (option == "D") {
-            let positionNeedDeleting = Number(prompt("Insert position that needs deleting") - 1);
-            if (positionNeedDeleting > 0 && positionNeedDeleting < stuff.length) {
-                stuff.splice(positionNeedDeleting, 1)
-                console.log(stuff)
-            }
-            else {
-                alert("not valid number. Please try again")
-            }   
-        }
-    }
-    else {
-        alert("Invalid Selection. Please Choose again");
-        CRUD();
-        
-    }
+
+let text = document.getElementById('demo');
+let button =document.getElementById('btn');
+let input = document.getElementById('input');
+let valueInput;
+const handleKeyUp = () => {
+    valueInput = input.value
+    text.innerHTML = valueInput
 }
+input.addEventListener('keyup', handleKeyUp)
+const refresh = () => {
+    text.innerHTML = '';
+}
+button.addEventListener('click', refresh)
